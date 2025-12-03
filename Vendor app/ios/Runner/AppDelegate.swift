8b2b3660e13572a76463750a3438d1e6a79dff3b
Monarchgrouptech <137780216+Monarchgrouptech@users.noreply.github.com>
2025-12-03 12:47:08 +0100

    Update Google Maps API key in AppDelegate

--- Vendor app/ios/Runner/AppDelegate.swift
+++ Vendor app/ios/Runner/AppDelegate.swift
@@ -10,7 +10,7 @@ import Firebase
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
-    GMSServices.provideAPIKey("AIzaSyCaCSJ0BZItSyXqBv8vpD1N4WBffJeKhLQ")
+    GMSServices.provideAPIKey("AIzaSyAVamyyQXuecYqP-WyQsVuNdi5mEwUnSGc")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
